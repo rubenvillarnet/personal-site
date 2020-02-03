a4e3e843ff4c6b33152fbc7899b0b22dfc871dff
Rubén Villar Grela <ruben@rubenvillar.net>
2020-02-03 20:18:12 +0100

    added Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,18 @@
 import App from 'next/app';
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
+import Router from 'next/router';
 import theme from '../styles/theme';
 import GlobalStyle from '../styles/GlobalStyle';
+import { initGA, logPageView } from '../utils/analytics';
 
 export default class MyApp extends App {
+  componentDidMount() {
+    initGA();
+    logPageView();
+    Router.events.on('routeChangeComplete', logPageView);
+  }
+
   render() {
     const { Component, pageProps } = this.props;
     return (
